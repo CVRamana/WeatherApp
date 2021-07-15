41028d064a9ff4c722e91e462e1592176106ac1f
Raman Verma <raman.verma@appinventiv.com>
2021-07-15 11:59:19 +0530

    Permission managed

--- src/container/WeatherHome.js
+++ src/container/WeatherHome.js
@@ -53,45 +53,15 @@ function WeatherHome() {
         Geocoder.from(
             lat, long)
             .then((res) => {
-
-                if (res?.results.length === 0) {
-                    let inCaseOfZeroResults = {}
-                    inCaseOfZeroResults = {
-                        formatted_address: res.plus_code?.compound_code,
-                        geometry: {
-                            location: {
-                                lat: coords?.latitude,
-                                lng: coords?.longitude
-                            }
-                        }
-                    }
-                    setState({ ...state, locationData: res.results[0], isLocationLoading: false })
-                    setTimeout(() => {
-                        if (fromCreatePost) {
-                            navigation.navigate(constants.screens.CREATE_POST, { postsLocation: inCaseOfZeroResults })
-                        } else {
-                            navigation.navigate(constants.screens.EDIT_POST, { postsLocation: inCaseOfZeroResults })
-                        }
-
-                    }, 600);
-
-                } else {
-                    setState({ ...state, locationData: res.results[0], isLocationLoading: false })
-                    setTimeout(() => {
-                        if (fromCreatePost) {
-                            navigation.navigate(constants.screens.CREATE_POST, { postsLocation: res.results[0] })
-                        } else {
-                            navigation.navigate(constants.screens.EDIT_POST, { postsLocation: res.results[0] })
-                        }
-
-                    }, 600);
-                }
+                setState({
+                    ...state,
+                    isLocationLoading: false
+                })
             })
-            .catch((erro) => {
+            .catch((error) => {
                 console.warn(error)
                 setState({
                     ...state,
-
                     isLocationLoading: false
                 })
             });
@@ -100,44 +70,44 @@ function WeatherHome() {
 
     const hasLocationPermission = async () => {
         if (Platform.OS === 'ios') {
-          const hasPermission = await hasPermissionIOS();
-          return hasPermission;
+            const hasPermission = await hasPermissionIOS();
+            return hasPermission;
         }
-    
+
         if (Platform.OS === 'android' && Platform.Version < 23) {
-          return true;
+            return true;
         }
-    
+
         const hasPermission = await PermissionsAndroid.check(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         );
-    
+
         if (hasPermission) {
-          return true;
+            return true;
         }
-    
+
         const status = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         );
-    
+
         if (status === PermissionsAndroid.RESULTS.GRANTED) {
-          return true;
+            return true;
         }
-    
+
         if (status === PermissionsAndroid.RESULTS.DENIED) {
-          ToastAndroid.show(
-            'Location permission denied by user.',
-            ToastAndroid.LONG,
-          );
+            ToastAndroid.show(
+                'Location permission denied by user.',
+                ToastAndroid.LONG,
+            );
         } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
-          ToastAndroid.show(
-            'Location permission revoked by user.',
-            ToastAndroid.LONG,
-          );
+            ToastAndroid.show(
+                'Location permission revoked by user.',
+                ToastAndroid.LONG,
+            );
         }
-    
+
         return false;
-      };
+    };
 
     return (
         < View >
